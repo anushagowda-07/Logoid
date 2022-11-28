5d02df7f29dd1babbe0772709d5004d4efe80a35
VeeRuby Developer <anushaagowda2001@gmail.com>
2022-11-28 09:56:50 +0530

    Add question 1

---
+++ Logoid/Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/DefaultCompany/Logoid/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DefaultCompany.Logoid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.DefaultCompany.Logoid";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
